8a872d340c3984d3a3daaf95fca3e687994cc3a8
lukejoliat <lukejoliat@gmail.com>
2023-03-14 14:13:34 -0400

    add other methods back

--- index.mjs
+++ index.mjs
@@ -4,8 +4,6 @@ import cors from "cors";
 
 const app = express();
 
-console.log('process.env', process.env)
-
 // Configure MySQL connection pool
 const pool = mysql.createPool({
   connectionLimit: 10,
@@ -25,23 +23,24 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.status(200).send("ok");
 });
+
 // Create
-// app.post("/notifications", (req, res) => {
-//   const { name, description } = req.body;
-//   const sql = "INSERT INTO NOTIFICATIONS (name, description) VALUES (?, ?)";
-//   pool.getConnection((err, connection) => {
-//     if (err) {
-//       throw err;
-//     }
-//     connection.query(sql, [name, description], (err, result) => {
-//       connection.release();
-//       if (err) {
-//         throw err;
-//       }
-//       res.status(201).json(result);
-//     });
-//   });
-// });
+app.post("/notifications", (req, res) => {
+  const { name, description } = req.body;
+  const sql = "INSERT INTO NOTIFICATIONS (name, description) VALUES (?, ?)";
+  pool.getConnection((err, connection) => {
+    if (err) {
+      throw err;
+    }
+    connection.query(sql, [name, description], (err, result) => {
+      connection.release();
+      if (err) {
+        throw err;
+      }
+      res.status(201).json(result);
+    });
+  });
+});
 
 // Read
 app.get("/notifications", (req, res) => {
@@ -61,60 +60,60 @@ app.get("/notifications", (req, res) => {
   });
 });
 
-// // Read
-// app.get("/notifications/:id", (req, res) => {
-//   const id = req.params.id;
-//   const sql = "SELECT * FROM NOTIFICATIONS WHERE id = ?";
-//   pool.getConnection((err, connection) => {
-//     if (err) {
-//       throw err;
-//     }
-//     connection.query(sql, [id], (err, result) => {
-//       connection.release();
-//       if (err) {
-//         throw err;
-//       }
-//       res.json(result);
-//     });
-//   });
-// });
+// Read
+app.get("/notifications/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM NOTIFICATIONS WHERE id = ?";
+  pool.getConnection((err, connection) => {
+    if (err) {
+      throw err;
+    }
+    connection.query(sql, [id], (err, result) => {
+      connection.release();
+      if (err) {
+        throw err;
+      }
+      res.json(result);
+    });
+  });
+});
 
-// // Update
-// app.put("/notifications/:id", (req, res) => {
-//   const id = req.params.id;
-//   const { name, description } = req.body;
-//   const sql = "UPDATE NOTIFICATIONS SET name = ?, description = ? WHERE id = ?";
-//   pool.getConnection((err, connection) => {
-//     if (err) {
-//       throw err;
-//     }
-//     connection.query(sql, [name, description, id], (err, result) => {
-//       connection.release();
-//       if (err) {
-//         throw err;
-//       }
-//       res.json(result);
-//     });
-//   });
-// });
+// Update
+app.put("/notifications/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, description } = req.body;
+  const sql = "UPDATE NOTIFICATIONS SET name = ?, description = ? WHERE id = ?";
+  pool.getConnection((err, connection) => {
+    if (err) {
+      throw err;
+    }
+    connection.query(sql, [name, description, id], (err, result) => {
+      connection.release();
+      if (err) {
+        throw err;
+      }
+      res.json(result);
+    });
+  });
+});
 
-// // Delete
-// app.delete("/notifications/:id", (req, res) => {
-//   const id = req.params.id;
-//   const sql = "DELETE FROM NOTIFICATIONS WHERE id = ?";
-//   pool.getConnection((err, connection) => {
-//     if (err) {
-//       throw err;
-//     }
-//     connection.query(sql, [id], (err, result) => {
-//       connection.release();
-//       if (err) {
-//         throw err;
-//       }
-//       res.json(result);
-//     });
-//   });
-// });
+// Delete
+app.delete("/notifications/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "DELETE FROM NOTIFICATIONS WHERE id = ?";
+  pool.getConnection((err, connection) => {
+    if (err) {
+      throw err;
+    }
+    connection.query(sql, [id], (err, result) => {
+      connection.release();
+      if (err) {
+        throw err;
+      }
+      res.json(result);
+    });
+  });
+});
 
 // Start server
 app.listen(8080, () => {
