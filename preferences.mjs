7561062e19ac8f4ee2cee24595aea24bd9dd8c16
lukejoliat <lukejoliat@gmail.com>
2023-06-16 11:20:48 -0400

    Latest update

--- preferences.mjs
+++ preferences.mjs
@@ -46,12 +46,16 @@ router.get("/:username", (req, res) => {
 router.get("/:username/notifications/:id", (req, res) => {
     const username = req.params.username
     const id = req.params.id
-    const sql = "SELECT * FROM PREFERENCES WHERE username = ? AND notification = ?";
+    const sql = `SELECT * FROM PREFERENCES p
+                LEFT JOIN NOTIFICATIONS 
+                ON p.notification = NOTIFICATIONS.id
+                WHERE username = ? 
+                AND notification = ?`;
     pool.getConnection((err, connection) => {
         if (err) {
             throw err;
         }
-        connection.query(sql, [username, id], (err, result) => {
+        connection.query(sql, [username, id, id], (err, result) => {
             connection.release();
             if (err) {
                 throw err;
